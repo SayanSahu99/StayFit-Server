b0799ece8f31a97459daee0512d4c8820589124a
SayanSahu99 <sayansovan@gmail.com>
2022-06-07 17:33:08 +0530

    Database connection

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var connectdb = require('./database/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require("dotenv").config();
+
 var app = express();
-const port = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// port and DB config
+const DATABASE_CONNECTION = process.env.DATABASE_URL;
+const DATABASE_PORT = process.env.DATABASE_PORT || 5000;
+
+// mongoose connection
+connectdb(DATABASE_CONNECTION, DATABASE_PORT);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -39,8 +51,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, (req, res) => {
-  console.log(`listening at port ${port}`);
+app.listen(PORT, (req, res) => {
+  console.log(`listening at port ${PORT}`);
 });
 
 module.exports = app;
