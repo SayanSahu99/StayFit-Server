e81f6660d80e69c1b0d2fdbf776ccd23e380126d
SayanSahu99 <sayansovan@gmail.com>
2022-06-10 20:21:17 +0530

    Health Profile Model

---
+++ models/profile.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Profile = mongoose.model('Profile', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    height: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    weight: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ['male', 'female'],
+    },
+    activity: {
+        type: String,
+        required: true,
+        enum: ['sedenatry', 'light', 'moderate', 'high'],
+    },
+    medical: {
+        type: String,
+        required: true,
+        enum: ['diabetes', 'thyroid', 'PCOS', 'cholestrol', 'physical injury', 'hypertension'],
+    }
+}));
+
+exports.Profile = Profile;
