9df42fb1f8581acbc01e4a79c0a282d317d84400
SayanSahu99 <sayansovan@gmail.com>
2022-07-09 20:03:25 +0530

    auth middleware added

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,19 @@
 const { check } = require('express-validator');
 const express = require('express');
 const { updateProfile, getProfile } = require('../controllers/profileController');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 // @route GET consumptions/
 // @desc Updates consumption list
-router.get('/', getProfile);
+router.get('/', auth, getProfile);
 
 // @route PUT consumptions/list
 // @desc Updates consumption list
 // @access Private
-router.put('/',
+router.post('/',
+    auth,
     updateProfile
 );
 
