c0b34b823715cf4d37835e8db497eb625fc2c95c
SayanSahu99 <sayansovan@gmail.com>
2022-07-09 20:03:08 +0530

    auth middleware added

--- routes/consumption.js
+++ routes/consumption.js
@@ -1,18 +1,20 @@
 const { check } = require('express-validator');
 const express = require('express');
 const { updateConsumptionList, getConsumptionList, getConsumptionListByDate } = require('../controllers/consumptionListController');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 // @route GET consumptions/
 // @desc Updates consumption list
-router.get('/list', getConsumptionList);
-router.get('/list/:date_string', getConsumptionListByDate);
+router.get('/list', auth, getConsumptionList);
+router.get('/list/:date_string', auth, getConsumptionListByDate);
 
 // @route PUT consumptions/list
 // @desc Updates consumption list
 // @access Private
 router.put('/list',
+    auth,
     check('consumptionList', 'Consumptions must be an array').isArray(),
     updateConsumptionList
 );
