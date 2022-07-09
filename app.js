5fbee6b07e04e27d6ce16fc312fc34608a96daf9
SayanSahu99 <sayansovan@gmail.com>
2022-07-09 19:59:36 +0530

    set url encoded to true

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
